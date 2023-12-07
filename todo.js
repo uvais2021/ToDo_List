a591d83007489c4ef94f62a5fb2155fafe8242f9
Uvais A <138773333+uvais2021@users.noreply.github.com>
2023-12-07 15:30:01 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,39 +1,31 @@
-// const addBtn = document.getElementById('add');
-// const removeBtn = document.getElementById('remove');
-// const results = document.getElementById('results');
-
-// addBtn.addEventListener('click', addItem => {
-//  const inputV = document.getElementById('InputValue');
-//     const List = document.createElement('li');
-//     List.innerHTML += `Item:${inputV.value}`;
-//     console.log(inputV.value)
-//     List.append(List.value)
-// })
-
-// function () {
-   
-// }
-
 
 const addBtn = document.getElementById('add');
 const removeBtn = document.getElementById('remove');
 const list = document.getElementById('list');
 
-addBtn.addEventListener('click', addItem);
 
+addBtn.addEventListener('click', addItem);
 function addItem() {
     const inputValue = document.getElementById('InputValue').value;
 
     if (inputValue.trim() !== '') {
         const listItem = document.createElement('li');
-        listItem.textContent = `Item: ${inputValue}`;
+        listItem.textContent = ` ${inputValue}`;
         list.appendChild(listItem);
         console.log(inputValue);
-
-        // Clear the input field after adding the item
-        // document.getElementById('InputValue').value = '';
+        document.getElementById('InputValue').value = '';
     } else {
-        // Handle the case where the input is empty
+ 
         alert('Please enter a value before adding.');
     }
 }
+
+removeBtn.addEventListener('click',removeItem);
+function removeItem(){
+   
+        if (list.lastChild) {
+            list.removeChild(list.lastChild);
+        } else {
+            alert('The list is empty.');
+        }
+    }
